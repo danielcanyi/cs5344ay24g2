922d66de000b75fb3ad842082cd41497aa9b8c90
Daniel <daniel.canyi@gmail.com>
2025-04-18 19:12:41 +0800

    fix: typo for asiatravelking constant var name

--- scrape.py
+++ scrape.py
@@ -218,7 +218,7 @@ def scrape_asiakingtravel() -> None:
                     lambda full_url: urllib.parse.urlparse(
                         full_url).path.replace("blog/", "")
                 ),
-                site_name=constants.SITE_NAME_ASIATRAVELKING
+                site_name=constants.SITE_NAME_ASIAKINGTRAVEL
             )
     print("[Scraping] done for asiakingtravel")
 
